3859cb6de4bfcc66300ca00c98d41fe0109b5bcc
Adelmostafa2008 <adel.0523035@gmail.com>
2025-11-01 18:17:34 +0200

    small mods

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae75ce7cbfb5f4741226bb27f5e94ea7fb7fb58e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34a234c1e4a3f674bfeb50f4515f5428ca779a78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
